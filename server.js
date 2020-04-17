c6ef5a9f040540ad6b46c158ebdc67f692a06b4b
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-17 17:47:09 -0600

    got rid of original html

--- server.js
+++ server.js
@@ -4,3 +4,26 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// server access to routes
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+// start server
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
