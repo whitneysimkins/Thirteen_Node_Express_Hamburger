ea517b5156b71f8dfcd9342ea30d130f3f624319
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-17 17:58:54 -0600

    redid the orm

---
+++ config/orm.js
@@ -0,0 +1,91 @@
+const connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+    const arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    const arr = [];
+
+    for (var key in ob) {
+        const value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+const orm = {
+    all: function (tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    delete: function (table, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE eaten = true;";
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
