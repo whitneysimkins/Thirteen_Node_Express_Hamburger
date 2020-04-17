9ffc6fc499b6b9dd9c6a69647f4c6b312bbae654
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-17 17:39:47 -0600

    building out database

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Swiss');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('California');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bacon');
